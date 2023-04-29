ca44aea633e7ad94cf315700c085b1f86d54ff98
Moowonn <rlaandnjs11@naver.com>
2023-04-29 15:28:19 +0900

    장소들 입력하여 저장해두는 코드

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-print('hi')
-print('')
-print('phython')
-print("f")
-print('')
+import random
+
+places = []
+
+while True:
+    place = input("장소를 입력하세요 (종료하려면 '끝'을 입력하세요): ")
+    if place == '끝':
+        break
+    places.append(place)
+    
+    # 출처 : chat gpt
+
