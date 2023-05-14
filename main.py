052902bd82c50dc94af32bd415228d2d8a7c53a1
ChoEChoU <lmtna99@gmail.com>
2023-05-14 21:55:00 +0900

    Update places.py, main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from places import places
+from places import Places
 import pandas as pd
 
 # csv 데이터 불러오기
 cafe_data = pd.read_csv('cafe_place_data.csv',encoding='utf-8')
 food_data = pd.read_csv('food_place_data.csv',encoding='utf-8')
 tour_data = pd.read_csv('tourist_place_data.csv',encoding='utf-8')
-walk_data = pd.read_csv('walk_place_data.csv',encoding='utf-8')
 
 # place class 생성 및 초기 설정
-place = places(cafe_data)
-place.init_place()
+place = Places(cafe_data, food_data, tour_data)
+place.init_cafe_place()
+place.init_food_place()
+place.init_tourist_place()
 
 # 장소 추천 함수 실행
 recommend_places = place.start_recommend()
