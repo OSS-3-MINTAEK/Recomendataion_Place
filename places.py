d69e52c76856e7b6a0a5f0d5328491808098ec51
Moowonn <rlaandnjs11@naver.com>
2023-06-02 11:10:54 +0900

    수정

--- places.py
+++ places.py
@@ -1,105 +1,94 @@
-import random
+import pandas as pd
+from geopy.geocoders import Nominatim
+
 class Places:
-    def __init__(self, cafe_data, food_data, tourist_data):
-        self.cafe_places_name = []
-        self.food_places_name = []
-        self.tourist_places_name = []
-        self.recommended_place = []
-        self.cafe_data = cafe_data
-        self.food_data = food_data
-        self.tourist_data = tourist_data
+    def __init__(self, food_data, cafe_data, hotel_data, tourist_data):
+        self.food_places = food_data
+        self.cafe_places = cafe_data
+        self.hotel_places = hotel_data
+        self.tourist_places = tourist_data
+        self.geolocator = Nominatim(user_agent="geopy")
 
-    def init_cafe_place(self):
-        place_name = self.cafe_data['이름'].values
-        for name in place_name:
-            self.add_cafe_place(name)
+    def convert_address_to_coordinates(self, address):
+        try:
+            location = self.geolocator.geocode(address)
+            if location:
+                return location.latitude, location.longitude
+        except:
+            pass
 
-    def init_food_place(self):
-        place_name = self.food_data['이름'].values
-        for name in place_name:
-            self.add_food_place(name)
+        return None, None
 
-    def init_tourist_place(self):
-        place_name = self.tourist_data['이름'].values
-        for name in place_name:
-            self.add_tourist_place(name)
+    def calculate_distance(self, lat1, lon1, lat2, lon2):
+        # Calculate the differences in latitude and longitude
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
 
-    def init_all(self):
-        self.init_cafe_place()
-        self.init_food_place()
-        self.init_tourist_place()
+        # Use the Pythagorean theorem to calculate the distance
+        distance = (dlat**2 + dlon**2)**0.5
 
-    def add_cafe_place(self, name):
-        self.cafe_places_name.append(name)
+        return distance
 
-    def add_food_place(self, name):
-        self.food_places_name.append(name)
+    def compare_distances(self, user_location):
+        user_lat, user_lon = self.convert_address_to_coordinates(user_location)
+        if user_lat is None or user_lon is None:
+            print("유효하지 않은 사용자 위치입니다.")
+            return
 
-    def add_tourist_place(self, name):
-        self.tourist_places_name.append(name)
+        distances = []
 
-    def get_cafe_places(self):
-        return self.cafe_places_name
-
-    def get_food_places(self):
-        return self.food_places_name
-
-    def get_tourist_places(self):
-        return self.tourist_places_name
-    
-    def print_recommeded_places(self):
-        for recommend_place in self.recommended_place:
-            print('장소: ', recommend_place)
-
-    def start_recommend(self):
-        # 추천 장소 리셋
-        self.recommended_place.clear()
-
-        # 추천 카페 랜덤 선택
-        random_cafe = random.choice(self.cafe_places_name)
-
-        # 추천 음식점 랜덤 선택
-        random_food = random.choice(self.food_places_name)
-
-        # 추천 관광지 랜덤 선택
-        random_tourist = random.choice(self.tourist_places_name)
-
-        # 추천 장소 리스트에 추가
-        self.recommended_place.append(random_cafe)
-        self.recommended_place.append(random_food)
-        self.recommended_place.append(random_tourist)
-
-        # 사용자에게 물어보기
-        while True:
-            self.print_recommeded_places()
-            check = self.user_check()
-            if check == True:
+        for index, row in self.food_places.iterrows():
+            food_lat, food_lon = self.convert_address_to_coordinates(row['위치'])
+            if food_lat is not None and food_lon is not None:
+                food_distance = self.calculate_distance(user_lat, user_lon, food_lat, food_lon)
+                distances.append(('음식점', row['이름'], food_distance))
                 break
 
-        # 리턴
-        return self.recommended_place
-    
-    def user_check(self):
-        true_false = False
-        check = input("Do you want to change the place? (yes / no) ")
-        if check == "yes":
-            change_type = input("Which place do you want to change? (cafe / restaurant / tour) ")
-            if change_type == "cafe":
-                self.recommended_place[0] = random.choice(self.cafe_places_name)
-            elif change_type == "restaurant":
-                self.recommended_place[1] = random.choice(self.food_places_name)
-            elif change_type == "tour":
-                self.recommended_place[2] = random.choice(self.tourist_places_name)
-            else:
-                print("Invalid input.Please choose either 'cafe', 'restaurant', or 'tour'.")
-        elif check == "no":
-            print("keep it")
-            true_false = True
-        else:
-            print("Invalid input. Please enter either 'yes' or 'no'.")
-        return true_false
-    
-    # 데이터 파일 경로
-cafe_data = 'cafe_place_data.csv'
-food_data = 'food_place_data.csv'
-tourist_data = 'tourist_place_data.csv'
+        for index, row in self.cafe_places.iterrows():
+            cafe_lat, cafe_lon = self.convert_address_to_coordinates(row['위치'])
+            if cafe_lat is not None and cafe_lon is not None:
+                cafe_distance = self.calculate_distance(user_lat, user_lon, cafe_lat, cafe_lon)
+                distances.append(('카페', row['이름'], cafe_distance))
+                break
+
+        for index, row in self.hotel_places.iterrows():
+            hotel_lat, hotel_lon = self.convert_address_to_coordinates(row['위치'])
+            if hotel_lat is not None and hotel_lon is not None:
+                hotel_distance = self.calculate_distance(user_lat, user_lon, hotel_lat, hotel_lon)
+                distances.append(('호텔', row['이름'], hotel_distance))
+                break
+
+        for index, row in self.tourist_places.iterrows():
+            tourist_lat, tourist_lon = self.convert_address_to_coordinates(row['위치'])
+            if tourist_lat is not None and tourist_lon is not None:
+                tourist_distance = self.calculate_distance(user_lat, user_lon, tourist_lat, tourist_lon)
+                distances.append(('관광지', row['이름'], tourist_distance))
+                break
+
+        distances.sort(key=lambda x: x[2])
+
+        print(f"사용자 위치({user_location})와의 거리:")
+        for distance in distances:
+            print(f"{distance[0]}: {distance[1]}, 거리: {distance[2]:.2f} km")
+
+
+# CSV 파일 경로
+food_csv = 'food_place_data.csv'
+cafe_csv = 'cafe_place_data.csv'
+hotel_csv = 'hotel_place_data.csv'
+tourist_csv = 'tourist_place_data.csv'
+
+# CSV 데이터 로드
+food_data = pd.read_csv(food_csv, encoding='utf-8')
+cafe_data = pd.read_csv(cafe_csv, encoding='utf-8')
+hotel_data = pd.read_csv(hotel_csv, encoding='utf-8')
+tourist_data = pd.read_csv(tourist_csv, encoding='utf-8')
+
+# Places 객체 생성
+places = Places(food_data, cafe_data, hotel_data, tourist_data)
+
+# 사용자 위치 입력
+user_location = "서울시 강남구"
+
+# 사용자 위치와 장소들 사이의 거리 비교
+places.compare_distances(user_location)
