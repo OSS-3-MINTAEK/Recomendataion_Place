d8febd7142c77e420cf22688f45a28702c332e3a
Moona <moonapark1126@gmail.com>
2023-06-07 19:55:18 +0900

    gui add

--- gui.py
+++ gui.py
@@ -41,13 +41,15 @@ def  process():
     result_cafe = near_user_cafe.sort_values(by='거리 차', ascending=True).head(1)[['이름', '거리 차']]
     result_food = near_user_food.sort_values(by='거리 차', ascending=True).head(1)[['이름', '거리 차']]
     result_tour = near_user_tour.sort_values(by='거리 차', ascending=True).head(1)[['이름', '거리 차']]
-   # result_hotel = near_user_hotel.sort_values(by='거리 차', ascending=True).head(1)[['이름', '거리 차']]
+    result_hotel = near_user_hotel.sort_values(by='거리 차', ascending=True).head(1)[['이름', '거리 차']]
     e1.delete(0,END) 
-    e1.insert(0,result_cafe)
+    e1.insert(0, result_cafe)
     e2.delete(0,END) 
     e2.insert(0,result_food)
     e3.delete(0,END) 
     e3.insert(0,result_tour)
+    e4.delete(0,END) 
+    e4.insert(0,result_hotel)
 
 # 위치 입력 받는 곳
 Label(window,text="제주도 장소 추천").grid(row=0)
@@ -57,17 +59,21 @@ put=Entry(window)
 put.grid(row=1,column=1)
 Button(window,text="입력",command=process).grid(row=1,column=2)
 
-Label(window,text="장소1:").grid(row=3,column=0)
-Label(window,text="장소2:").grid(row=4,column=0)
-Label(window,text="장소3:").grid(row=5,column=0)
+Label(window,text="카 페:").grid(row=3,column=0)
+Label(window,text="음식점:").grid(row=4,column=0)
+Label(window,text="관광지:").grid(row=5,column=0)
+Label(window,text="숙 소:").grid(row=6,column=0)
 
 e1=Entry(window)
 e2=Entry(window)
 e3=Entry(window)
+e4=Entry(window)
 e1.grid(row=3,column=1)
 e2.grid(row=4,column=1)
 e3.grid(row=5,column=1)
+e4.grid(row=6,column=1)
 Button(window,text="변경").grid(row=3,column=2)
 Button(window,text="변경").grid(row=4,column=2)
 Button(window,text="변경").grid(row=5,column=2)
+Button(window,text="변경").grid(row=6,column=2)
 window.mainloop()
